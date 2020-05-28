7658e3ee92d4f28f61f8885f3169b9c2240cc48b
MohammedAlabd <mu96abd@gmail.com>
2020-05-28 16:22:13 +0300

    add dotenv and aylien_textapi

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,17 +1,28 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+  let text = document.querySelector("#text").value;
+  // check what text was put into the form field
+  // checkForName(text)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        console.log(res)
-        document.getElementById('results').innerHTML = res.message
-    })
+  const configObj = {
+    METHOD: {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: text,
+    },
+  };
+
+  console.log("::: Form Submitted :::");
+  fetch("http://localhost:8081/post", configObj)
+    .then((res) => res.json())
+    .then(function (res) {
+      console.log(res);
+      document.getElementById("results").innerHTML = res.message;
+    });
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,39 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-var cors = require('cors')
+const path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const aylien = require("aylien_textapi");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const app = express()
+dotenv.config();
+const app = express();
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-app.use(cors())
+app.use(cors());
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+// set aylien API credentias
+const textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY,
+});
+
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+  console.log("Example app listening on port 8081!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
+app.post("/post", function (req, res) {
+  res.send(req.body);
+  // res.send(mockAPIResponse)
+});
