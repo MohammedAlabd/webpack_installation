12e70fa0010bd0bfaa47dea3258d539ce110f0a3
MohammedAlabd <mu96abd@gmail.com>
2020-05-31 15:28:18 +0300

    remove unused file

--- src/client/index.js
+++ src/client/index.js
@@ -1,20 +1,12 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
-import { renderResults } from './js/renderResult'
-import './styles/resets.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
-import './styles/header.scss'
-import './styles/base.scss'
-
-
-console.log(checkForName);
+import { handleSubmit } from "./js/formHandler";
+import { renderResults } from "./js/renderResult";
+import "./styles/resets.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+import "./styles/base.scss";
 
 // alert("I EXIST")
 console.log("CHANGE!!");
 
-export {
-    checkForName,
-    handleSubmit,
-    renderResults
-}
+export { handleSubmit, renderResults };
