d92de6240af21b76337f89a63932410cbdfff5b0
MohammedAlabd <mu96abd@gmail.com>
2020-06-01 17:44:41 +0300

    add testing for formHandler.js file

---
+++ src/client/js/formHandler.test.js
@@ -0,0 +1,41 @@
+import "babel-polyfill"
+import {handleSubmit} from "./formHandler"
+import jsdom from 'jsdom';
+const JSDOM = jsdom.JSDOM;
+const dom = new JSDOM(`
+<form class="" onsubmit="return Client.handleSubmit(event)">
+    <label for="text"> enter you text below: </label>
+    <textarea id="text" type="text" rows="5" cols="100" name="text" value=""> </textarea>
+    <br>
+    <input type="submit" name="" value="submit" onclick="return Client.handleSubmit(event)"
+        onsubmit="return Client.handleSubmit(event)">
+  </form>
+`);
+const unMockedFetch = global.fetch;
+
+beforeAll(() => {
+    global.document = dom.window.document
+    
+  global.fetch = () =>
+    Promise.resolve({
+      json: () => ({
+        test: "test string",
+      }),
+    });
+  global.Client = {
+    renderResults: () => null,
+  };
+});
+
+afterAll(() => {
+  global.fetch = unMockedFetch;
+});
+
+describe("handleSubmit", () => {   
+    it("works", async () => {
+      const json = await handleSubmit({ preventDefault: () => null });
+      console.log(json)
+      expect(json).toEqual(1);
+    });
+  });
+  
